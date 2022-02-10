e07d5dc57d31dd59b3560915febfb8dd9945a542
DichMS <86953785+DichMS@users.noreply.github.com>
2022-02-10 09:52:28 +0300

    Initial commit

---
+++ registr.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>Razr_1</title>
+</head>
+    <body>
+        <header>
+            <h1>Разработка В-П</h1>
+        </header>
+        <nav>
+            <ol>
+                <li><a href="#">Главная</a> </li>
+                <li><a href="#">Регистрация</a> </li>
+                <li><a href="#">Авторизация</a></li>
+            </ol>
+        </nav>
+        <main>
+            <div class="content">
+                <form action="#">
+                    <input type="text" name="login" placeholder="login" required>
+                    <input type="password" name="password" placeholder="password" required>
+
+                    <input type="submit" name="send" value="Регистрация">
+                    <input type="submit" name="to_index" value="На главную">
+                </form>
+            </div>
+        </main>
+        <footer>
+            <!--<p class = "name">
+                Головин Андрей
+            </p>
+            <p>
+                группа 2011
+            </p>
+            <p><a class = "VK_a" href="https://vk.com/dich_ms">VK</a></p>
+            <p class = "uni">
+                Университет Дубна
+            </p>
+            <p>
+                Осень, 2021
+            </p>-->
+        </footer>
+    </body>
+</html>
+
+<?php
